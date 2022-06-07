1eb473e87c217b6d490a8bcfc001e03fe7dff6ff
annofi-umesh <umesh.gm@annofi.com>
2022-06-07 10:18:26 +0545

    add auth to utils

---
+++ utils/auth/withAuth.ts
@@ -0,0 +1,12 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export function withAuth(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  next: () => any
+) {
+  if (!req.headers.authentication) {
+    res.status(401).json({ name: "Error" });
+  }
+  return next();
+}
